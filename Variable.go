18a2b2b584e0a739bde7a005a752c94358cedae4
beta <rofiq.beta@gmail.com>
2020-11-21 02:38:45 +0700

    commit1

---
+++ Variable.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nama string
+
+	nama = "Mochammad"
+	fmt.Println(nama)
+
+	nama = "Chusnul"
+	fmt.Println(nama)
+
+	var nama1 = "Rofiq"
+	fmt.Println(nama1)
+
+	umur := 23
+	fmt.Println(umur)
+
+	var (
+		namalengkap   = "Mochammad Chusnul Rofiq"
+		namapanggilan = "Rofiq"
+	)
+
+	fmt.Println(namalengkap)
+	fmt.Println(namapanggilan)
+}
